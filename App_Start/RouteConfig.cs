01d2fb2a70ed719c2a502c3cdf9833fb5fcc9e64
vgl-spec <bautista.vergel.agripa@gmail.com>
2026-04-01 23:47:47 +0800

    Recover Lara pages and re-integrate Jom/Trisha2 feature set on Main

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -14,9 +14,15 @@ namespace ArtZada
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Root",
+                url: "",
+                defaults: new { controller = "Landing", action = "Landing" }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Landing", action = "Landing", id = UrlParameter.Optional }
             );
         }
     }
--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -10,34 +10,144 @@ namespace ArtZada.Controllers
     {
         public ActionResult Index()
         {
+            ViewBag.Title = "Store";
+            ViewBag.Page = "store";
             return View();
         }
 
-        public ActionResult Message()
+        public ActionResult Cart()
         {
+            ViewBag.Title = "Cart";
+            ViewBag.Page = "Cart";
             return View();
+        }
+
+        public ActionResult Message(string withUserId = null)
+        {
+            ViewBag.Title = "Messaging";
+            ViewBag.Page = "message";
+            ViewBag.WithUserId = withUserId;
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult SendMessage(string receiverId, string messageText)
+        {
+            if (!string.IsNullOrWhiteSpace(messageText)
+                && !string.IsNullOrWhiteSpace(receiverId))
+            {
+                // TODO: persist chat messages to DB.
+            }
+
+            return RedirectToAction("Message", new { withUserId = receiverId });
+        }
+
+        public ActionResult Notifications()
+        {
+            ViewBag.Title = "Notifications";
+            ViewBag.Page = "notifications";
+            return View();
+        }
+
+        public ActionResult Analytics()
+        {
+            ViewBag.Title = "Analytics";
+            ViewBag.Page = "analytics";
+            ViewBag.Revenue = "100.00";
+            return View();
+        }
+
+        public ActionResult Account()
+        {
+            ViewBag.Title = "Account";
+            ViewBag.Page = "account";
+            ViewBag.Username = "PAMILI IS LOVE";
+            ViewBag.IsSeller = true;
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult EditUsername(string NewUsername)
+        {
+            if (!string.IsNullOrWhiteSpace(NewUsername))
+            {
+                // TODO: update username in DB.
+            }
+
+            return RedirectToAction("Account");
+        }
+
+        public ActionResult ChangeProfile()
+        {
+            ViewBag.Title = "Change Profile";
+            ViewBag.Page = "account";
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult ChangeProfile(HttpPostedFileBase avatarFile)
+        {
+            if (avatarFile != null && avatarFile.ContentLength > 0)
+            {
+                // TODO: save avatar file and update user profile.
+            }
+
+            return RedirectToAction("Account");
+        }
+
+        public ActionResult ToggleSeller()
+        {
+            return RedirectToAction("Account");
+        }
+
+        public ActionResult SwitchToBuyer()
+        {
+            return RedirectToAction("Account");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult SignOut()
+        {
+            System.Web.Security.FormsAuthentication.SignOut();
+            Session.Clear();
+            Session.Abandon();
+            return RedirectToAction("Index", "Home");
         }
 
         public ActionResult Product()
         {
             return View();
         }
+
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
 
             return View();
         }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+            return View();
+        }
+
         public ActionResult SellerProf()
         {
             return View();
         }
+
         public ActionResult AdminAccount()
         {
-            ViewBag.AdminUsername = ""; // Replace with your session/db value
-            ViewBag.ProfileImageUrl = "";        // Replace with actual image path if available
+            ViewBag.AdminUsername = "";
+            ViewBag.ProfileImageUrl = "";
             return View();
         }
+
         public ActionResult AdminEditAccount()
         {
             return View();
@@ -57,7 +167,6 @@ namespace ArtZada.Controllers
         {
             return View();
         }
-
         
     }
 }
