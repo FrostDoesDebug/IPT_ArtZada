1ac96f8b203a5eafad3150d64c60ea093c03a9ab
vgl-spec <bautista.vergel.agripa@gmail.com>
2026-04-01 21:35:44 +0800

    Revert branch content to 31241cc snapshot

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -14,15 +14,9 @@ namespace ArtZada
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
-                name: "Root",
-                url: "",
-                defaults: new { controller = "Landing", action = "Landing" }
-            );
-
-            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Landing", action = "Landing", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
